66e909e055f02b6d924309fc8df8c6ab7481ab38
Rafael De Luca <deluca1712@gmail.com>
2022-12-04 18:11:25 -0300

    Testando salvar no arquivo de criptografia

--- vigenere.py
+++ vigenere.py
@@ -1,3 +1,6 @@
+import sys
+import os
+
 # função para amortecer a chave de criptografia
 def _amortecer_chave(textopuro, chave):
     chave_amortecida = ''
@@ -41,15 +44,58 @@ def descriptografar(textocifrado,chave):
         textopuro += _descriptografar_criptografar_caracter(caracter_textocifrado, caracter_chave, mode='descriptografar')
     return textopuro
 
-# informar um texto para criptografar e a chave
-textopuro = input('Informe um texto para criptografar com a cifra de vegenere: ')
-chave = input('Informe a chave de criptografia: ')
 
-textocifrado = criptografar(textopuro,chave)
-texto_descriptogradado = descriptografar(textocifrado,chave) 
+# verificar se foi informado 4 parâmetros no console do terminal
+if len(sys.argv) == 4:
 
-# imprimir a cifra criptografada e depois descriptografada
-print(f'Texto crifrado: {textocifrado}')
-print(f'Texto Descriptogradado: {texto_descriptogradado}')
+    nome_arquivo_criptografia = sys.argv[0]
+    arquivo_texto = sys.argv[1]
+    chave = sys.argv[2]
+    tipo = sys.argv[3]
 
+    # verifica se arquivo de texto tem extensão .txt
+    if arquivo_texto[len(arquivo_texto)-4:] != '.txt':
+        print('O segundo parâmetro tem que ser um arquivo do tipo texto')
+
+    # verificar se foi passada a chave de criptografia (se ela não é nula)
+    elif chave == "":
+        print('O terceiro parâmetro deve ser a chave de criptografia não nula')
     
+    elif tipo !='criptografar' and tipo !='descriptografar':
+        print('O terceiro parametro tem que ser a palavra criptografar ou descriptografar.\nDependendo da ação que desejas.')
+
+    # se tudo foi validado ok. vai executar o script
+
+    else:
+        arquivo = open(arquivo_texto)
+        textopuro= arquivo.read()
+        arquivo.close()
+
+        # informar um texto para criptografar e a chave
+        #textopuro = input('Informe um texto para criptografar com a cifra de vegenere: ')
+        #chave = input('Informe a chave de criptografia: ')
+
+        if tipo == 'criptografar':        
+
+            textocifrado = criptografar(textopuro,chave)
+            texto_descriptogradado = descriptografar(textocifrado,chave)
+
+            arquivo_texto = arquivo_texto[:len(arquivo_texto) - 4] + '.txt' 
+
+            # criar um novo arquivo e salvar nele o texto criptografado
+            arquivo_novo = open(arquivo_texto,'w+')
+            arquivo_novo.write(textocifrado)
+            arquivo_novo.close()
+
+            # imprimindo na tela o texto cifrado que foi salvo no arquivo
+            print(f'Texto crifrado: {textocifrado}')
+            print(f'Texto Descriptogradado: {texto_descriptogradado}')
+        elif tipo == 'descriptografar':
+            
+            print('fazer o que tem que fazer se for pra descriptografar')       
+
+                
+
+else:   
+    print('Você deve informar o nome do arquivo e 4 parâmetros, conforme o comando abaixo')
+    print('python <vinegere.py> <arquivo.txt> <chave de criptografia> <criptografar ou descriptografar>')
